0e6b04f7efc13cf8b493ee8cdab68130978fe795
Dmitry Davydov <haudvd@gmail.com>
2014-02-27 19:37:29 +0400

    remove create action from news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,13 +1,4 @@
 class NewsController < ApplicationController
-  def create
-    @news = News.new params[:news]
-    if @news.save
-      redirect_to root_path, flash: :success
-    else
-      render action: :new, flash: :error
-    end
-  end
-
   def index
     @news = News.order('published_at DESC').page(params[:page]).per(5)
   end
