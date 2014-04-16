fdd946641794c985fe432369ed1570b27ede9d1e
pavel <kalashnikovisme@gmail.com>
2014-04-17 03:24:08 +0400

    fix test

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
+    history = create :page
+    history.slug = "history"
+    history.save
     get :index
     assert_response :success
   end
