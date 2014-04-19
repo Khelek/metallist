3d756a3deff8d14f1b72a34bc076ca299ad2f2f1
pavel <kalashnikovisme@gmail.com>
2014-04-19 11:10:05 +0400

    fix users edit

--- app/types/user_edit_type.rb
+++ app/types/user_edit_type.rb
@@ -1,15 +1,27 @@
 class UserEditType < User
   include BasicType
 
-  attr_accessible :password, :password_confirmation
+  attr_accessor :old_password
+  attr_accessible :old_password, :password, :password_confirmation
+  #has_secure_password
 
-  validates_each :password do |record, attr, value|
-    unless value =~ /^.{5,30}$/
-      record.errors.add(attr, I18n.t(:password_not_valid, scope: [:activerecord, :errors, :messages]))
+  validates :first_name,  presence: true
+  validates :last_name,  presence: true
+  validates_each :old_password do |record, attr, value|
+    @old_password_is_empty = (value.nil? or value.empty?)
+    if !@old_password_is_empty and !User.find(record.id).authenticate(value)
+      record.errors.add(attr, I18n.t(:password_is_wrong, scope: [:activerecord, :errors, :messages]))
     end
   end
-  validates :password, presence: true, confirmation: true
-  validates :password_confirmation, presence: true
+  validates_each :password do |record, attr, value|
+    unless @old_password_is_empty and (value.nil? or value.empty?)
+      unless value =~ /^.{8,30}$/ and value =~ /^.*[a-z]+.*$/ and
+          value =~ /^.*[A-Z]+.*$/ and value =~ /^.*[0-9]+.*$/ and
+          value =~ /^.*[`~\!\@\#\$\%^&*\(\)_+=\-"{}\[\]\/\\|;:,.?<>]+.*$/
+        record.errors.add(attr, I18n.t(:password_not_valid, scope: [:activerecord, :errors, :messages]))
+      end
+    end
+  end
 
   def email=(email)
     write_attribute(:email, email.mb_chars.downcase)
